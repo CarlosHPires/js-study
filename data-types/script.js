5f6c6ff4b1ecaf6435a3f2d98f7b6d48b6f02fd9
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-12-03 17:04:13 -0300

    feat: data types

---
+++ data-types/script.js
@@ -0,0 +1,14 @@
+let frase = "Carlos é bonitão";
+let number = "33";
+let age = 21;
+
+let name = "Carlos";
+let lastName = "Pires";
+let completeName = `${name} ${lastName}`;
+
+let sentence = "It's Time";
+
+console.log(typeof completeName);
+console.log(completeName);
+
+console.log(sentence);
