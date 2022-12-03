c0ecbf5e456040e08cef5232b409895ae7b38f56
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-12-03 17:03:43 -0300

    feat: everything is objects

---
+++ everything-is-object/script.js
@@ -0,0 +1,20 @@
+// Exercicio
+
+// nomeie 3 propriedades ou métodos de strings
+
+var nome = "Carlos";
+//fixed;
+//length;
+//replace;
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector(".btn");
+// addEventListener
+// appendChild
+// id
+// innerHtml
+// outerHtml
+console.log(btn);
+
+// busque na web um objeto (método) capaz de interagir com o clip
+// clipboard é a parte do seu computador que lida com o CTRL + C
