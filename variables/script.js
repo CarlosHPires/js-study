741491deb09f177397a0498db00a9c5731392257
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-12-03 17:03:02 -0300

    feat: variables

---
+++ variables/script.js
@@ -0,0 +1,21 @@
+let nome = "Carlos";
+let idade = 21;
+let comidaFavorita;
+comidaFavorita = "Salmão";
+
+let namorada;
+let jogoFavorito;
+let pet;
+let placaDeVideo;
+let amigo;
+
+console.log(
+  nome,
+  idade,
+  comidaFavorita,
+  namorada,
+  jogoFavorito,
+  pet,
+  placaDeVideo,
+  amigo
+);
