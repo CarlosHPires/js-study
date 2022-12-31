d5f5a95d386c970fd2dc19c8a0ca41ea816f3457
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2022-12-31 00:42:37 -0300

    fix: I just rename files

--- 9.foreach-e-arrow-function/script-aula.js
+++ 9.foreach-e-arrow-function/script-aula.js
@@ -1,29 +1,18 @@
-// const imgs = document.querySelectorAll('img');
+// const imgs = document.querySelectorAll("img");
 
-// imgs.forEach(function(item, index, array){
-//   // console.log(item, index, array)
+// imgs.forEach(function (item, index, array) {
+//   // console.log(item, index, array);
 // });
 
-const titulos = document.getElementsByClassName('titulo');
-const titulosArray = Array.from(titulos);
+// const titulos = document.getElementsByClassName("titulo");
+// const titulosArray = Array.from(titulos);
 
-// console.log(titulos);
-// console.log(titulosArray);
-
-titulosArray.forEach(function(item, index, array){
-  // console.log(item, index, array);
-});
-
-const imgs = document.querySelectorAll('img');
-
-// let i = 0;
-// imgs.forEach((item) => {
-//   console.log(i++)
+// titulosArray.forEach(function (item, index, array) {
+//   console.log(item, index, array);
 // });
 
-let i = 0;
-imgs.forEach(function() { 
-  console.log(i++)
-});
+// const imgs = document.querySelectorAll("img");
 
-imgs.forEach(() => console.log(i++));
+// imgs.forEach((item, index) => {
+//   console.log(item, index);
+// });
--- 9.foreach-e-arrow-function/script.js
+++ 9.foreach-e-arrow-function/script.js
@@ -1,6 +1,5 @@
 // Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);
+const paragrafos = document.querySelectorAll("p");
 
 paragrafos.forEach((item) => {
   console.log(item);
@@ -10,9 +9,8 @@ paragrafos.forEach((item) => {
 paragrafos.forEach((item) => {
   console.log(item.innerText);
 });
-
 // Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
+const imgs = document.querySelectorAll("img");
 
 imgs.forEach((item, index) => {
   console.log(item, index);
