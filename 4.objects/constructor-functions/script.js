91397423c9f94c6353038bffaba7ba64ef537255
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2023-01-25 01:10:21 -0300

    feat: constructor function object script

--- 4.objects/constructor-functions/script.js
+++ 4.objects/constructor-functions/script.js
@@ -1,12 +1,20 @@
-const carro = {
-  marca: "Marca",
-  preco: 0,
-};
+// const carro = {
+//   marca: "Marca",
+//   preco: 0,
+// };
 
-const honda = carro;
-honda.marca = "Honda";
-honda.preco = 4000;
+// const honda = carro;
+// honda.marca = "Honda";
+// honda.preco = 4000;
 
-const fiat = carro;
-fiat.marca = "Fiat";
-fiat.preco = 3000;
+// const fiat = carro;
+// fiat.marca = "Fiat";
+// fiat.preco = 3000;
+
+function Carro(marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro("Honda", 50000);
+const fiat = new Carro("Fiat", 45000);
