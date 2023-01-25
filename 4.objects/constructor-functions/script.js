a5754e05f1747ad2e96c770db86325abf336f9d0
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2023-01-24 23:57:35 -0300

    feat: just a object

---
+++ 4.objects/constructor-functions/script.js
@@ -0,0 +1,12 @@
+const carro = {
+  marca: "Marca",
+  preco: 0,
+};
+
+const honda = carro;
+honda.marca = "Honda";
+honda.preco = 4000;
+
+const fiat = carro;
+fiat.marca = "Fiat";
+fiat.preco = 3000;
