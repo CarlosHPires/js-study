3ded8f1cc3b846e06d4ec70fcc2c71276f0adfed
CarlosPires3b <Caik_oliveirapires@hotmail.com>
2023-01-29 23:14:31 -0300

    feat: native constructors

--- 4.objects/prototype/script.js
+++ 4.objects/prototype/script.js
@@ -18,7 +18,39 @@ Pessoa.prototype.nadar = function () {
   return this.nome + " Pessoa nadou";
 };
 
-const pessoa1 = new Pessoa("Carlos", 21);
+// const pessoa1 = new Pessoa("Carlos", 21);
 
-console.log(Pessoa.prototype);
-console.log(pessoa1.prototype);
+// console.log(Pessoa.prototype);
+// console.log(pessoa1.prototype);
+
+const pais = "Brasil";
+const cidade = new String("Rio");
+
+pais.charAt(0);
+cidade.charAt(0);
+
+const listaAnimais = ["Cachoro", "Gato", "Cavalo"];
+
+const lista = document.querySelectorAll("li");
+
+// Transforma em uma array
+const listaArray = Array.prototype.slice.call(lista);
+const listaArray2 = Array.from(lista);
+
+const Carro = {
+  marca: "ford",
+  preco: 2000,
+  andar() {
+    return "Oi";
+  },
+};
+
+// Carro // Object
+// Carro.marca // String
+// Carro.preco // Number
+// Carro.acelerar // Function
+// Carro.acelerar() // Boolean
+// Carro.marca.charAt // Function
+// Carro.marca.charAt(0) // String
+
+// Verifique o nome do construtor com: .constructor.name
